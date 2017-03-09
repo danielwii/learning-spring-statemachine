f9ba303687b313675e41b4d355fce19d6618d90a
Daniel Wei <danielwii0326@gmail.com>
2017-03-09 18:27:07 +0800

    init

---
+++ src/main/java/statemachine/Application.java
@@ -0,0 +1,96 @@
+package statemachine;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.EnableStateMachine;
+import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.listener.StateMachineListener;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.state.State;
+
+import javax.annotation.Resource;
+import java.util.EnumSet;
+
+/**
+ * @author daniel
+ * @since 09/03/2017
+ */
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Resource
+    private StateMachine<States, Events> stateMachine;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        stateMachine.start();
+        stateMachine.sendEvent(Events.BOOTSTRAP);
+        stateMachine.sendEvent(Events.SHUT);
+    }
+}
+
+@Slf4j
+@Configuration
+@EnableStateMachine
+class Config extends EnumStateMachineConfigurerAdapter<States, Events> {
+
+    @Override
+    public void configure(StateMachineConfigurationConfigurer<States, Events> config) throws Exception {
+        config.withConfiguration()
+                .autoStartup(true)
+                .listener(listener());
+    }
+
+    @Override
+    public void configure(StateMachineStateConfigurer<States, Events> states) throws Exception {
+        states.withStates()
+                .initial(States.WAITING)
+                .end(States.DOWN)
+                .states(EnumSet.allOf(States.class));
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<States, Events> transitions) throws Exception {
+        transitions.withExternal()
+                .source(States.WAITING).target(States.UP)
+                .event(Events.BOOTSTRAP).and()
+                .withExternal()
+                .source(States.UP).target(States.DOWN)
+                .event(Events.SHUT);
+    }
+
+    @Bean
+    public StateMachineListener<States, Events> listener() {
+        return new StateMachineListenerAdapter<States, Events>() {
+            @Override
+            public void stateChanged(State<States, Events> from, State<States, Events> to) {
+                log.info("State change to {}", to.getId());
+            }
+        };
+    }
+
+}
+
+enum States {
+    WAITING,
+    UP,
+    DOWN,
+}
+
+enum Events {
+    BOOTSTRAP,
+    SHUT,
+}
